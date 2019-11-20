b6a009393ab2a2783b7e1bd36272d255d76b6f4b
Natthawit Janjaroen <natwit442@gmail.com>
2019-11-21 01:17:39 +0700

    login adjustment

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -32,40 +32,40 @@ const LoginScreen = ({ navigation }) => {
     loadFont();
   });
 
-  async function facebookLogIn() {
-    try {
-      const {
-        type,
-        token,
-        expires,
-        permissions,
-        declinedPermissions
-      } = await Facebook.logInWithReadPermissionsAsync("544837729681231", {
-        permissions: ["public_profile"]
-      });
-      if (type === "success") {
-        // Get the user's name using Facebook's Graph API
-        const response = await fetch(
-          `https://graph.facebook.com/me?access_token=${token}`
-        );
+  const signInWithFacebook = async () => {
+    const appId = "523523305157137";
+    const permissions = ["public_profile", "email"]; // Permissions required, consult Facebook docs
 
-        const data = await response.json();
-        const credential = await firebase.auth.FacebookAuthProvider.credential(
-          token
-        );
-        firebase.auth().signInWithCredential(credential);
-        navigation.navigate("App");
-
-        // Alert.alert("Logged in!", `Hi ${data.name}!`); // Uncomment this line to see your display name na ja
-        // Good place to redirect the account somewhere
-      } else {
-        // type === 'cancel'
-        // User doesn't grant the access using Facebook
+    const { type, token } = await Facebook.logInWithReadPermissionsAsync(
+      appId,
+      {
+        permissions
       }
-    } catch ({ message }) {
-      alert(`Facebook Login Error: ${message}`);
+    );
+
+    switch (type) {
+      case "success": {
+        await firebase
+          .auth()
+          .setPersistence(firebase.auth.Auth.Persistence.LOCAL); // Set persistent auth state
+        const credential = firebase.auth.FacebookAuthProvider.credential(token);
+        const facebookProfileData = await firebase
+          .auth()
+          .signInWithCredential(credential); // Sign in with Facebook credential
+        console.log(facebookProfileData);
+        break;
+        // Do something with Facebook profile data
+        // OR you have subscribed to auth state change, authStateChange handler will process the profile data
+      }
+      case "cancel": {
+        console.log("cancel");
+        break;
+      }
+      default: {
+        console.log("default case triggered");
+      }
     }
-  }
+  };
 
   return (
     <LinearGradient
@@ -80,7 +80,7 @@ const LoginScreen = ({ navigation }) => {
           <Text style={styles.textLogo}> Initiative </Text>
         </>
       ) : null}
-      <TouchableOpacity onPress={facebookLogIn}>
+      <TouchableOpacity onPress={signInWithFacebook}>
         <View style={styles.facebookLogoContainer}>
           <FacebookLogo style={styles.facebookLogo} width={30} height={30} />
           <Text style={styles.facebookLogoText}> Sign In With Facebook</Text>
