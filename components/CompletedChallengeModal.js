398e9ddcbe9f2c059599414b1aede9c30459b583
Suchanun Piriyasatit <s.piriyasatit@gmail.com>
2019-11-21 01:07:17 +0700

    adjust flex

--- components/CompletedChallengeModal.js
+++ components/CompletedChallengeModal.js
@@ -21,16 +21,22 @@ class CompletedChallengeModal extends Component {
                        animationIn="zoomInDown"
                 >
                     <View style={{
-                        flex: 1,
-                        // flexDirection: 'column',
+                        backgroundColor: 'white',
+                        flex: 0.5,
                         justifyContent: 'center',
                         alignItems: 'center'}}>
                         <SvgUri width="50%" height="50%" uri="https://firebasestorage.googleapis.com/v0/b/the-green-initiative.appspot.com/o/winner%201.svg?alt=media&token=a80ecd9f-e959-4975-9e70-e8c1d74cd353"/>
-                        {/*<Text>Challenge Completed!</Text>*/}
-                        <H2 style={{ color: 'white' }}>Challenge Completed!</H2>
-                        <H3 style={{ color: 'white' }}>30 points received</H3>
+                        <H2 style={{ color: 'darkcyan' }}>Challenge Completed!</H2>
+                        <Text style={{ color: 'darkorange' }}>30 points received</Text>
                         <Button title="Hide modal" onPress={this.toggleModal} />
                     </View>
+                    {/*<View style={{*/}
+                        {/*flex: ,*/}
+                        {/*// flexDirection: 'column',*/}
+                        {/*justifyContent: 'center',*/}
+                        {/*alignItems: 'center'}}>*/}
+                        {/*/!*<H3 style={{ color: 'white' }}>30 points received</H3>*!/*/}
+                    {/*</View>*/}
                 </Modal>
             </View>
         );
