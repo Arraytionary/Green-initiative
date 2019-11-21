113183b4176d16603f80b4e270047c4cdb756399
Suchanun Piriyasatit <s.piriyasatit@gmail.com>
2019-11-21 21:00:02 +0700

    change flex of modal

--- components/CompletedChallengeModal.js
+++ components/CompletedChallengeModal.js
@@ -6,7 +6,7 @@ import { SvgUri } from 'react-native-svg';
 
 class CompletedChallengeModal extends Component {
     state = {
-        isModalVisible: false
+        isModalVisible: true
     };
 
     toggleModal = () => {
@@ -15,28 +15,25 @@ class CompletedChallengeModal extends Component {
 
     render() {
         return (
-            <View style={{ flex: 1 }}>
-                <Button title="Show modal" onPress={this.toggleModal} />
+            <View >
+                {/*style={{ flex: 1 }}>*/}
+                {/*<Button title="Show modal" onPress={this.toggleModal} />*/}
                 <Modal isVisible={this.state.isModalVisible}
                        animationIn="zoomInDown"
                 >
                     <View style={{
                         backgroundColor: 'white',
-                        flex: 0.5,
+                        // flex: 0.5,
                         justifyContent: 'center',
                         alignItems: 'center'}}>
                         <SvgUri width="50%" height="50%" uri="https://firebasestorage.googleapis.com/v0/b/the-green-initiative.appspot.com/o/winner%201.svg?alt=media&token=a80ecd9f-e959-4975-9e70-e8c1d74cd353"/>
+                             {/*="https://firebasestorage.googleapis.com/v0/b/the-green-initiative.appspot.com/o/winner%201.svg?alt=media&token=a80ecd9f-e959-4975-9e70-e8c1d74cd353"/>*/}
                         <H2 style={{ color: 'darkcyan' }}>Challenge Completed!</H2>
                         <Text style={{ color: 'darkorange' }}>30 points received</Text>
                         <Button title="Hide modal" onPress={this.toggleModal} />
+
                     </View>
-                    {/*<View style={{*/}
-                        {/*flex: ,*/}
-                        {/*// flexDirection: 'column',*/}
-                        {/*justifyContent: 'center',*/}
-                        {/*alignItems: 'center'}}>*/}
-                        {/*/!*<H3 style={{ color: 'white' }}>30 points received</H3>*!/*/}
-                    {/*</View>*/}
+
                 </Modal>
             </View>
         );
