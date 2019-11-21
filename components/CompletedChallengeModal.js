70b8411a827e6c5fb5acdcce037cd3540e733568
Suchanun Piriyasatit <s.piriyasatit@gmail.com>
2019-11-21 23:25:17 +0700

    delete comments

--- components/CompletedChallengeModal.js
+++ components/CompletedChallengeModal.js
@@ -16,8 +16,6 @@ class CompletedChallengeModal extends Component {
     render() {
         return (
             <View >
-                {/*style={{ flex: 1 }}>*/}
-                {/*<Button title="Show modal" onPress={this.toggleModal} />*/}
                 <Modal isVisible={this.state.isModalVisible}
                        animationIn="zoomInDown"
                 >
@@ -27,7 +25,6 @@ class CompletedChallengeModal extends Component {
                         justifyContent: 'center',
                         alignItems: 'center'}}>
                         <SvgUri width="50%" height="50%" uri="https://firebasestorage.googleapis.com/v0/b/the-green-initiative.appspot.com/o/winner%201.svg?alt=media&token=a80ecd9f-e959-4975-9e70-e8c1d74cd353"/>
-                             {/*="https://firebasestorage.googleapis.com/v0/b/the-green-initiative.appspot.com/o/winner%201.svg?alt=media&token=a80ecd9f-e959-4975-9e70-e8c1d74cd353"/>*/}
                         <H2 style={{ color: 'darkcyan' }}>Challenge Completed!</H2>
                         <Text style={{ color: 'darkorange' }}>30 points received</Text>
                         <Button title="Hide modal" onPress={this.toggleModal} />
