0b554238eec813cfc39e39dd63bc1c85fc1ef6d1
Suchanun Piriyasatit <s.piriyasatit@gmail.com>
2019-11-24 16:54:42 +0700

    adjust button

--- components/CompletedChallengeModal.js
+++ components/CompletedChallengeModal.js
@@ -4,29 +4,21 @@ import { H1, H2, H3 } from 'native-base';
 import Modal from "react-native-modal";
 import { SvgUri } from 'react-native-svg';
 import * as Sharing from 'expo-sharing';
-// import { FileSystem } from 'expo';
 import * as FileSystem from 'expo-file-system';
 const image_source ='https://firebasestorage.googleapis.com/v0/b/the-green-initiative.appspot.com/o/The%20Green%20Initiative.png?alt=media&token=dc455bd9-b5bb-4222-b693-f25abb5d0311'
-    // 'https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80';
+
 
 class CompletedChallengeModal extends Component {
     constructor(props) {
         super(props);
-        // const shareLinkContent = {
-        //     contentType: 'link',
-        //     contentUrl: 'https://www.facebook.com/',
-        //     contentDescription: 'Facebook sharing is easy!'
-        // };
-
         this.state =  {
             isModalVisible: false,
-            // shareLinkContent: shareLinkContent,
         };
     }
-
-
     toggleModal = () => {
+
         this.setState({ isModalVisible: !this.state.isModalVisible });
+        console.log("toggleModal to isModalVisible == ",this.state.isModalVisible);
     };
     onShare(){
         console.log('FileSystem.documentDirectory ', FileSystem.documentDirectory)
@@ -34,7 +26,6 @@ class CompletedChallengeModal extends Component {
         FileSystem.downloadAsync(
             image_source,
             FileSystem.documentDirectory + 'joinme.png'
-            // + '.jpeg'
         )
             .then(({ uri }) => {
                 console.log('Finished downloading to ', uri);
@@ -47,6 +38,7 @@ class CompletedChallengeModal extends Component {
     }
     render() {
         return (
+
             <View >
                 <Button title="Complete" onPress={this.toggleModal} />
                 <Modal isVisible={this.state.isModalVisible}
@@ -58,12 +50,11 @@ class CompletedChallengeModal extends Component {
                         justifyContent: 'center',
                         alignItems: 'center'}}>
                         <SvgUri width="50%" height="50%" uri="https://firebasestorage.googleapis.com/v0/b/the-green-initiative.appspot.com/o/winner%201.svg?alt=media&token=a80ecd9f-e959-4975-9e70-e8c1d74cd353"/>
-                        <H2 style={{ color: 'darkcyan' }}>Challenge Completed!</H2>
-                        <Text style={{ color: 'darkorange' }}>30 points received</Text>
-                        <Button title="Hide modal" onPress={this.toggleModal} />
-                        <TouchableHighlight onPress={this.onShare}>
-                            <Text style={styles.shareText}>Share</Text>
-                        </TouchableHighlight>
+                        <H1 style={{ color: 'darkcyan' }}>Challenge Completed!</H1>
+                        <H3 style={{ color: 'darkorange'}}>30 points received</H3>
+
+                        <Button title="Share" onPress={this.onShare.bind(this)} />
+                        <Button title="Back to Challenge" onPress={this.toggleModal} />
                     </View>
 
 
@@ -72,17 +63,17 @@ class CompletedChallengeModal extends Component {
         );
     }
 }
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    shareText: {
-        fontSize: 20,
-        margin: 10,
-    },
-});
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         backgroundColor: '#F5FCFF',
+//     },
+//     shareText: {
+//         fontSize: 20,
+//         margin: 10,
+//     },
+// });
 
 export default CompletedChallengeModal;
