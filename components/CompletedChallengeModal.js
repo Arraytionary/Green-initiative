6a748e63b1d5fd45b0261baeef54b8627f3162f5
Suchanun Piriyasatit <s.piriyasatit@gmail.com>
2019-11-24 15:46:23 +0700

    add sharing joinme image

--- components/CompletedChallengeModal.js
+++ components/CompletedChallengeModal.js
@@ -3,21 +3,24 @@ import { Text, Button, TouchableHighlight, View, Alert,AppRegistry, StyleSheet} 
 import { H1, H2, H3 } from 'native-base';
 import Modal from "react-native-modal";
 import { SvgUri } from 'react-native-svg';
-// import {AppRegistry, StyleSheet, Text, TouchableHighlight, View,} from 'react-native';
-import {LoginButton, ShareDialog} from 'react-native-fbsdk';
+import * as Sharing from 'expo-sharing';
+// import { FileSystem } from 'expo';
+import * as FileSystem from 'expo-file-system';
+const image_source ='https://firebasestorage.googleapis.com/v0/b/the-green-initiative.appspot.com/o/The%20Green%20Initiative.png?alt=media&token=dc455bd9-b5bb-4222-b693-f25abb5d0311'
+    // 'https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80';
 
 class CompletedChallengeModal extends Component {
     constructor(props) {
         super(props);
-        const shareLinkContent = {
-            contentType: 'link',
-            contentUrl: 'https://www.facebook.com/',
-            contentDescription: 'Facebook sharing is easy!'
-        };
+        // const shareLinkContent = {
+        //     contentType: 'link',
+        //     contentUrl: 'https://www.facebook.com/',
+        //     contentDescription: 'Facebook sharing is easy!'
+        // };
 
         this.state =  {
             isModalVisible: false,
-            shareLinkContent: shareLinkContent,
+            // shareLinkContent: shareLinkContent,
         };
     }
 
@@ -25,30 +28,23 @@ class CompletedChallengeModal extends Component {
     toggleModal = () => {
         this.setState({ isModalVisible: !this.state.isModalVisible });
     };
-    shareLinkWithShareDialog() {
-        var tmp = this;
+    onShare(){
+        console.log('FileSystem.documentDirectory ', FileSystem.documentDirectory)
+        console.log('image_source',image_source);
+        FileSystem.downloadAsync(
+            image_source,
+            FileSystem.documentDirectory + 'joinme.png'
+            // + '.jpeg'
+        )
+            .then(({ uri }) => {
+                console.log('Finished downloading to ', uri);
 
-        console.log(tmp.state.shareLinkContent);
-        ShareDialog.canShow(this.state.shareLinkContent).then(
-            function(canShow) {
-                if (canShow) {
-                    return ShareDialog.show(tmp.state.shareLinkContent);
-                }
-            }
-        ).then(
-            function(result) {
-                if (result.isCancelled) {
-                    alert('Share cancelled');
-                } else {
-                    alert('Share success with postId: ' + result.postId);
-                }
-            },
-            function(error) {
-                alert('Share fail with error: ' + error);
-            }
-        );
+                Sharing.shareAsync(uri);
+            })
+            .catch(error => {
+                console.error(error);
+            });
     }
-
     render() {
         return (
             <View >
@@ -65,8 +61,8 @@ class CompletedChallengeModal extends Component {
                         <H2 style={{ color: 'darkcyan' }}>Challenge Completed!</H2>
                         <Text style={{ color: 'darkorange' }}>30 points received</Text>
                         <Button title="Hide modal" onPress={this.toggleModal} />
-                        <TouchableHighlight onPress={this.shareLinkWithShareDialog.bind(this)}>
-                            <Text style={styles.shareText}>Share link with ShareDialog</Text>
+                        <TouchableHighlight onPress={this.onShare}>
+                            <Text style={styles.shareText}>Share</Text>
                         </TouchableHighlight>
                     </View>
 
