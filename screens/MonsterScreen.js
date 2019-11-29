945478815a512ccea1763b9f6e2d1e59a22e8213
Suchanun Piriyasatit <s.piriyasatit@gmail.com>
2019-11-29 16:27:18 +0700

    remove testing in moster screen

--- screens/MonsterScreen.js
+++ screens/MonsterScreen.js
@@ -1,14 +1,13 @@
 import React, {Component}  from 'react';
 import { Text, Button} from 'native-base';
 import Constants from 'expo-constants';
-import CompletedChallengeModal from '../components/CompletedChallengeModal'
 export default class MonsterScreen extends Component{
 
     render(){
 
         return (
-            <CompletedChallengeModal />
-            // <Text style={{ paddingTop: Constants.statusBarHeight }}> monster </Text>
+
+             <Text style={{ paddingTop: Constants.statusBarHeight }}> monster </Text>
         )
     }
 
