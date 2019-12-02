67080b83cf0c6fb8ac0f0b518ba704d2da14faa2
tctawan <chaeyklinthestawan@gmail.com>
2019-12-02 13:10:33 +0700

    update point in challengeScreen

--- screens/ChallengesScreen.js
+++ screens/ChallengesScreen.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react';
-import CompleteChallengeModal from '../components/CompletedChallengeModal'
 import {
   View,
   Text,
@@ -15,6 +14,7 @@ import { SvgUri } from 'react-native-svg';
 import firebase from 'firebase';
 import CardFlip from 'react-native-card-flip';
 import Constants from 'expo-constants';
+import CompleteChallengeModal from '../components/CompletedChallengeModal';
 
 import ProfilePhoto from '../assets/icons/profile_photo.svg';
 
@@ -46,12 +46,19 @@ const styles = StyleSheet.create({
 });
 
 const ChallengesScreen = props => {
-  const [userName, setUserName] = useState('Mapring');
+  const [userName, setUserName] = useState('');
+  const [totalPoints, setTotalPoints] = useState(0);
   const [photoUrl, setPhotoUrl] = useState('');
   useEffect(() => {
     if (firebase.auth().currentUser) {
       setPhotoUrl(firebase.auth().currentUser.photoURL);
       setUserName(firebase.auth().currentUser.displayName.split(' ')[0]);
+      firebase
+        .firestore()
+        .collection('users')
+        .doc(firebase.auth().currentUser.uid)
+        .get()
+        .then(doc => setTotalPoints(doc.data().totalPoints));
     }
   }, []);
   const [challenges] = useState([
@@ -65,8 +72,7 @@ const ChallengesScreen = props => {
         'https://firebasestorage.googleapis.com/v0/b/the-green-initiative.appspot.com/o/plastic_bag.svg?alt=media&token=df9473c1-fb96-4971-b921-8ab74310bdd8',
       description:
         'It takes about 450 years just for one plastic bottle to break down !',
-        points: 20,
-
+      points: 20,
     },
     {
       challengeId: 2,
@@ -81,23 +87,21 @@ const ChallengesScreen = props => {
         'https://firebasestorage.googleapis.com/v0/b/the-green-initiative.appspot.com/o/school_bus.svg?alt=media&token=5f37545e-9d72-4950-8497-8b77815e7abb',
       description:
         'Cars release approximately 333 million tons of carbon dioxide into the atmosphere annually, which is 20 percent of the world’s total',
-        points: 100,
+      points: 100,
     },
     {
       challengeId: 3,
-        backgroundColor: '#ff8047',
-        buttonColor: '#ff6937',
-        title: 'Challenges 3',
-        subtitle: 'Say no to plastic straw',
-        image:"https://firebasestorage.googleapis.com/v0/b/the-green-initiative.appspot.com/o/noStraw.svg?alt=media&token=4f080de9-4e14-4ded-abb3-9b4d89f96a4e",
-        description:"Plastic straws are among the top 10 contributors to plastic marine debris across the globe and most recycling machines aren’t capable of recycling straws, given their size.",
-        points: 100,
-       // image: "https://firebasestorage.googleapis.com/v0/b/the-green-initiative.appspot.com/o/pngtree-hand-drawn-no-plastic-straws-doodle-icon-png-image_1725678-removebg-preview(2).png?alt=media&token=03edffb4-9415-45f5-91f1-7e7b35029d2a"
-
-
-
-
-    }
+      backgroundColor: '#ff8047',
+      buttonColor: '#ff6937',
+      title: 'Challenges 3',
+      subtitle: 'Say no to plastic straw',
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/the-green-initiative.appspot.com/o/noStraw.svg?alt=media&token=4f080de9-4e14-4ded-abb3-9b4d89f96a4e',
+      description:
+        'Plastic straws are among the top 10 contributors to plastic marine debris across the globe and most recycling machines aren’t capable of recycling straws, given their size.',
+      points: 100,
+      // image: "https://firebasestorage.googleapis.com/v0/b/the-green-initiative.appspot.com/o/pngtree-hand-drawn-no-plastic-straws-doodle-icon-png-image_1725678-removebg-preview(2).png?alt=media&token=03edffb4-9415-45f5-91f1-7e7b35029d2a"
+    },
   ]);
   const card = useRef([]);
 
@@ -141,15 +145,17 @@ const ChallengesScreen = props => {
                   <SvgUri width="80%" height="80%" uri={image} />
                 </View>
                 <View style={{ alignItems: 'flex-end' }}>
-                  <CompleteChallengeModal challengeId={item.challengeId}
-                                          points={item.points}
-                                          buttonStyle={styles.button}
-                                          buttonColor={buttonColor}/>
-                  {/*<Button*/}
-                    {/*style={[styles.button, { backgroundColor: buttonColor }]}*/}
-                  {/*>*/}
-                    {/*<Text style={{ color: 'white' }}>Complete</Text>*/}
-                  {/*</Button>*/}
+                  <CompleteChallengeModal
+                    challengeId={item.challengeId}
+                    points={item.points}
+                    buttonStyle={styles.button}
+                    buttonColor={buttonColor}
+                  />
+                  {/* <Button */}
+                  {/* style={[styles.button, { backgroundColor: buttonColor }]} */}
+                  {/* > */}
+                  {/* <Text style={{ color: 'white' }}>Complete</Text> */}
+                  {/* </Button> */}
                 </View>
               </View>
             </View>
@@ -199,7 +205,7 @@ const ChallengesScreen = props => {
       >
         <View>
           <H1 style={{ fontWeight: 'bold' }}>Hello, {userName}</H1>
-          <Text style={{ marginTop: 6 }}>You have 9999 points</Text>
+          <Text style={{ marginTop: 6 }}>You have {totalPoints} points</Text>
         </View>
         <TouchableWithoutFeedback
           onPress={() => props.navigation.navigate('Profile')}
